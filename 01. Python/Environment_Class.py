b47e6786745c87eb99b36958cc849ea85df121e3
ignacio.brottier <ibrottier@itba.edu.ar>
2021-01-28 16:46:12 -0300

    First Commit - starting custom environment

---
+++ 01. Python/Environment_Class.py
@@ -0,0 +1,60 @@
+# -*- coding: utf-8 -*-
+"""
+------------------------------------------------------------------------------------------------------------------------
+Reinforcement Learning Environment for 2048 Puzzle Game
+------------------------------------------------------------------------------------------------------------------------
+Date: January 28 of 2021
+Considerations:
+    -
+Authors: Ignacio Brottier González
+"""
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Importing libraries and necessary functions
+# ----------------------------------------------------------------------------------------------------------------------
+from gym import Env
+from gym.spaces import Discrete, Box
+import numpy as np
+import random
+
+# ----------------------------------------------------------------------------------------------------------------------
+# Open AI RL Environment Class
+# ----------------------------------------------------------------------------------------------------------------------
+
+
+class TwentyFortyEightEnvironment(Env):
+    def __init__(self):
+        # Action Space : Down, Left, Up, Right
+        self.action_space = Discrete(4)
+
+        # Observation Space : 4x4 Matrix
+        self.observation_space = Box(
+            low=np.array([[0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0],
+                          [0, 0, 0, 0]]),
+            high=np.array([[999999, 999999, 999999, 999999],
+                           [999999, 999999, 999999, 999999],
+                           [999999, 999999, 999999, 999999],
+                           [999999, 999999, 999999, 999999]]),
+            shape=(4, 4),
+            dtype=int)
+
+        # Starting State : Zero survived turns -> objective is to survive as many turns as possible
+        self.state = np.array([[0, 0, 0, 0],
+                               [0, 0, 0, 0],
+                               [0, 0, 0, 0],
+                               [0, 0, 0, 0]])
+        self.turn = 0
+
+    def step(self, action):
+        self.turn += 1
+        reward = self.turn  # Reward = number of turns survived
+
+
+    def render(self, mode='human'):
+        pass
+
+    def reset(self):
+        pass
+
