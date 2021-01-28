db883179f38331b692717c724683fd286695e7da
ignacio.brottier <ibrottier@itba.edu.ar>
2021-01-28 17:16:41 -0300

    generate new number and check if done

--- 01. Python/Environment_Class.py
+++ 01. Python/Environment_Class.py
@@ -51,6 +51,52 @@ class TwentyFortyEightEnvironment(Env):
         self.turn += 1
         reward = self.turn  # Reward = number of turns survived
 
+        self.generate_new_number()
+        done = self.check_if_done()
+
+    def generate_new_number(self):
+        """
+        Finds all empty cells in the matrix and fills on of them with either 2 or 4
+        """
+        new_number = random.randrange(1, 3, 1) * 2
+
+        empty_positions = []
+        for row in range(len(self.observation_space.shape[0])):
+            for col in range(len(self.observation_space[1])):
+                if self.observation_space[row][col] == 0:
+                    empty_positions.append([row, col])
+
+        position = empty_positions[random.randrange(0, len(empty_positions) + 1, 1)]
+
+        self.observation_space[position[0]][position[1]] = new_number
+
+    def check_if_done(self):
+        """
+        Check if there's any available move to continue playing
+        :return: done: bool
+        """
+        done = True
+        for row in range(len(self.observation_space.shape[0])):
+            for col in range(len(self.observation_space[1])):
+
+                # If theres an empty cell, the game is not over
+                if self.observation_space[row][col] == 0:
+                    done = False
+
+                if row > 0:
+                    if self.observation_space[row][col] == self.observation_space[row - 1][col]:
+                        done = False
+                if row < self.observation_space.shape[0]:
+                    if self.observation_space[row][col] == self.observation_space[row + 1][col]:
+                        done = False
+                if col > 0:
+                    if self.observation_space[row][col] == self.observation_space[row][col - 1]:
+                        done = False
+                if col < self.observation_space.shape[1]:
+                    if self.observation_space[row][col] == self.observation_space[row][col + 1]:
+                        done = False
+
+        return done
 
     def render(self, mode='human'):
         pass
